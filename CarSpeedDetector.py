cb43d852ae2070a44f513c2b816b67622de06cb7
Brandon Wong <brandon@Brandons-MacBook-Air.local>
2026-06-09 15:17:21 +0800

    Changed video and now troubleshooting fpsand resolution issues

--- CarSpeedDetector.py
+++ CarSpeedDetector.py
@@ -10,10 +10,11 @@ model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(weights=WEIGH
 model.eval()
 
 CONFIDENCE_THRESHOLD = 0.5
-FRAME_RATE = 33
-PPM = 8.8
+FRAME_RATE = 31
+PPM = 10
 SPEED_LIMIT = 50
 FINE_AMOUNT = 25
+SCALE = 0.25
 
 def estimate_speed(pos1, pos2, PPM, FRAME_RATE, frames_elapsed):
     dist_pixels = math.sqrt((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2)
@@ -24,6 +25,7 @@ def estimate_speed(pos1, pos2, PPM, FRAME_RATE, frames_elapsed):
 
 def detect_car(frame, FRAME_RATE, PPM, frame_count, car_tracker):
     global pos_list_prev
+    
     img_tensor = torchvision.transforms.ToTensor()(frame)
     with torch.inference_mode():
         preds = model(img_tensor.unsqueeze(dim=0))
@@ -36,30 +38,33 @@ def detect_car(frame, FRAME_RATE, PPM, frame_count, car_tracker):
                 pos_list = [centroid_x, centroid_y]
 
                 if i not in car_tracker:
-                    car_tracker[i] = {"pos": pos_list, "frame": frame_count,"speed": None}
+                    car_tracker[i] = {"pos": pos_list, "frame": frame_count,"speed": None, "last_update": frame_count}
                     speed_text = "calculating..."
 
                 else:
                     pos1 = car_tracker[i]["pos"]
                     frame_started  = car_tracker[i]["frame"]
                     frames_elapsed = frame_count - frame_started
+                    frames_since_update = frame_count - car_tracker[i]["last_update"]
 
-                    if frames_elapsed >= 3:   # wait at least 10 frames for stable reading
+                    if frames_elapsed >= 4 and (car_tracker[i]["speed"] is None or frames_since_update >= 30):   # wait at least 10 frames for stable reading
                         speed = estimate_speed(pos1, pos_list, PPM, FRAME_RATE, frames_elapsed)
                         car_tracker[i]["speed"] = speed
                         speed_text = f"{speed:.1f} km/h"
 
+                        if speed >= SPEED_LIMIT:
+                            issue_ticket(speed)
+                            print("Speeding ticket issued")
+                    car_tracker[i]["pos"] = pos_list
+                    car_tracker[i]["frame"] = frame_count
                         
-                    else:
-                        speed_text = "calculating..."
                         
                     # if pos_list_prev is not None:
                     #     speed = estimate_speed(pos_list_prev, pos_list, PPM, FRAME_RATE, frames_elapsed)
                     #     if speed >= SPEED_LIMIT:
                     #         issue_ticket(speed)
                     #         print("Speeding ticket issued")
-                    car_tracker[i]["pos"] = pos_list
-                    car_tracker[i]["frame"] = frame_count
+                    
                     
                 stored_speed = car_tracker[i].get("speed")
                 if stored_speed is not None:
@@ -67,11 +72,13 @@ def detect_car(frame, FRAME_RATE, PPM, frame_count, car_tracker):
                     if speed >= SPEED_LIMIT:
                         issue_ticket(speed)
                         print("Speeding ticket issued")
-                    speed_text = f"{stored_speed:.1f}"
+                    speed_text = f"{stored_speed:.1f}km/h"
                 else:
                     speed = 0
-                    cv.putText(frame, f"Speed: {speed_text}", (x1,y1 + 20), cv.FONT_HERSHEY_SIMPLEX, 3, (0,255,0),2)
-                    pos_list_prev = pos_list
+                    speed_text = "calculating..."
+                
+                cv.putText(frame, f"Speed: {speed_text}", (x1,y1 + 20), cv.FONT_HERSHEY_SIMPLEX, 3, (0,255,0),2)
+                pos_list_prev = pos_list
         return frame
     
 def issue_ticket(speed):
@@ -85,7 +92,7 @@ def issue_ticket(speed):
 frame_count = 0 
 car_tracker = {}
 
-cap = cv.VideoCapture("/Users/brandon/Downloads/Cars Driving NEW.mov")
+cap = cv.VideoCapture('/Users/brandon/Downloads/NEW Moving Cars.mov')
 if not cap.isOpened():
     print("Error: Unable to load video")
     exit()
@@ -95,10 +102,17 @@ while cap.isOpened():
     if not ret:
         break
     frame_count += 1
+    
+    if frame_count % 2 != 0:   # skip odd frames
+        continue
+    
+    frame = cv.resize(frame, (0,0), fx=SCALE, fy=SCALE)
+    
     processed_frame = detect_car(frame, FRAME_RATE, PPM, frame_count, car_tracker)
 
     
-    cv.imshow("Cars Driving", processed_frame)
+    cv.imshow("Cars Driving",frame)
+    cv.waitKey(int(1000 / FRAME_RATE))
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
